13fa9f32274d8ec4ace2f359b77bcbf75aadb577
Restu <restu.meilandani48@smk.belajar.id>
2025-04-11 11:23:44 +0700

    jetstram

---
+++ Coba % Template/backend/database/migrations/2025_04_11_025439_create_games_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->bigint('created_by');
+            $table->timestamp('reated_at');
+            $table->timestamp('updated_at');
+            $table->timestamp('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
