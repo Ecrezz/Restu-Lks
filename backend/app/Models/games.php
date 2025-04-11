e31c281c3162d643c491c45cc7ce38ca809edb33
Restu <restu.meilandani48@smk.belajar.id>
2025-04-11 10:11:57 +0700

    membuat model

--- backend/app/Models/games.php
+++ backend/app/Models/games.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class games extends Model
 {
-    //
+    protected $table = 'games' ;
+    protected $fillable = ['id', 'title', 'slug', 'description', 'created_by', 'created_at', 'updated_at', 'deleted_at'];
 }
