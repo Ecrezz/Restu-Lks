b8223d996bd0a6497599729d85ef3a50eb00bd99
Restu <restu.meilandani48@smk.belajar.id>
2025-04-12 09:03:25 +0700

    Mengedit kode Welcome

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+ 
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        laravel(['resources/js/app.jsx']),
+        react(),
+    ],
+});
